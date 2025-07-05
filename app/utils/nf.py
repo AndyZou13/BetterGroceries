500c74c4e1a6311fe97bc5928e2eb2d5d6f3f376
Andy Zou <andyzou1302@gmail.com>
2025-07-04 23:04:52 -0400

    Update nf.py
    changed price from string to float
    isKG
    added engine to directly add df to db

--- app/utils/nf.py
+++ app/utils/nf.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from sqlalchemy import create_engine
 
 import pandas as pd
 import time
@@ -35,16 +36,18 @@ def freshScrape():
                 salePrice = topContainer.find_elements(By.CSS_SELECTOR, "span[data-testid='sale-price']")
                 link = veg.find_element(By.CSS_SELECTOR, "a.chakra-linkbox__overlay").get_attribute("href")
                 picture = veg.find_element(By.CSS_SELECTOR, 'img.chakra-image').get_attribute('src')
+                isKG = False
                 if salePrice:
                     if 'about' in salePrice[0].text:
                         salePrice = productPriceQuant.text[productPriceQuant.text.index('$'):productPriceQuant.text.index('/')]
-                        salePrice += '/KG'
+                        isKG = True
                     else:
                         salePrice = salePrice[0].text[salePrice[0].text.index('$'):]
                     category.append({
                         "Source": "NF",
                         "Product Name": productName,
-                        "Price": salePrice,
+                        "Price": float(salePrice[1:]),
+                        "isKG" : isKG,
                         "Link": link,
                         "Picture": picture,
                         "Date": pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -53,11 +56,12 @@ def freshScrape():
                     productPrice = topContainer.find_element(By.CSS_SELECTOR, "span[data-testid='regular-price']").text
                     if 'about' in productPrice:
                         productPrice = productPriceQuant.text[productPriceQuant.text.index('$'):productPriceQuant.text.index('/')]
-                        productPrice += '/KG'
+                        isKG = True
                     category.append({
                         "Source": "NF",
                         "Product Name": productName,
-                        "Price": productPrice,
+                        "Price": float(productPrice[1:]),
+                        "isKG" : isKG,
                         "Link": link,
                         "Picture": picture,
                         "Date": pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -67,3 +71,7 @@ freshScrape()
           
 df = pd.DataFrame(category)
 df.to_csv("nf.csv", index=False)
+
+engine = create_engine("postgresql+psycopg2://username:password@localhost:5432/rpidb")
+
+df.to_sql("your_table", engine, if_exists="append", index=False)
